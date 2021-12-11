1d60e12bbc8028d3028d1199ac9b35c302e29d93
Jeboczek <kanakuba123359@gmail.com>
2021-12-11 18:20:21 +0100

    Add Firebase

--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { initializeApp } from "firebase/app";
+
+// TODO: Provide your firebase config here
+const firebaseConfig = {
+  apiKey: "AIzaSyBv7DZXblFNT1LWF5ukKQ-AvdVH1__wgUw",
+  authDomain: "vue-chat-c04db.firebaseapp.com",
+  databaseURL: "https://vue-chat-c04db-default-rtdb.firebaseio.com",
+  projectId: "vue-chat-c04db",
+  storageBucket: "vue-chat-c04db.appspot.com",
+  messagingSenderId: "197907026099",
+  appId: "1:197907026099:web:9494e94de90794e02a60c6"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
 createApp(App).use(store).use(router).mount('#app')
