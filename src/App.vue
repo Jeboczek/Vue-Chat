afe0a3cccd1d485db5feaba7771bb0a6109f7a96
Jeboczek <kanakuba123359@gmail.com>
2021-12-11 18:18:20 +0100

    Attach RoomNav to App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
+  <RoomNav />
   <router-view/>
 </template>
 
+<script>
+export default {
+  name: "App",
+  components: {
+    RoomNav: require("@/components/RoomNav.vue").default
+  }
+}
+</script>
+
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap');
+
+  *{
+    box-sizing: border-box;
+    font-family: "Lato", sans-serif;
+  }
+
+  body{
+    margin: 0;
+  }
+
+  .container{
+    border-width: 2px;
+    border-style: solid;
+    border-color: black;  
+  }
 </style>
---
+++ src/components/RoomNav.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="room-nav">
+        <h1>Rooms</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RoomNav"
+}
+</script>
+
+<style lang="scss" scoped>
+    div.room-nav{
+        width: 20%;
+        min-height: 100vh;
+        padding: 20px 20px;
+        text-align: center;
+
+        h1:first-child{
+            margin-top: 0;
+        }
+    }
+</style>
