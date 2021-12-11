b1f7476e0c757923206fe2b7758f24676107c5ce
Jeboczek <kanakuba123359@gmail.com>
2021-12-11 18:56:34 +0100

    Add RoomLink with sample room

--- src/components/RoomNav.vue
+++ src/components/RoomNav.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="room-nav">
         <h1>Rooms</h1>
+        <SearchBar />
+        <ul>
+            <RoomLink v-for="room in rooms" :key="room['name']" :room="room" />
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-    name: "RoomNav"
+    name: "RoomNav",
+    data(){
+        return {
+            rooms: [{"name": "MyRoom", "icon": "fab fa-vuejs", "color": "green"}]
+        }
+    },
+    components: {
+        "RoomLink": require("@/components/RoomNav/RoomLink").default
+    }
 }
 </script>
 
@@ -20,5 +32,10 @@ export default {
         h1:first-child{
             margin-top: 0;
         }
+    }
+
+    ul {
+        list-style-type: none;
+        padding-left: 0;
     }
 </style>
---
+++ src/components/RoomNav/RoomLink.vue
@@ -0,0 +1,50 @@
+<template>
+    <li :style="getLiStyle">
+        <div class="room-icon">
+            <i :class="room['icon']"></i>
+        </div>
+        {{ room["name"] }}
+    </li>
+</template>
+
+<script>
+export default {
+    name: "RoomLink",
+    props: {
+        room: Object,
+    },
+    computed: {
+        getLiStyle(){
+            return {
+                "--icon-color": this.room["color"]
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    li{
+        cursor: pointer;
+        gap: 30px;
+        display: flex;
+        align-items: center;
+
+        font-size: 1.2em;
+
+        div.room-icon{
+            width: 45px;
+            height: 45px;
+            background-color: var(--icon-color) !important;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 100px;
+            
+            i{
+                font-size: 1.3em !important;
+            }
+        }
+    }
+
+</style>
